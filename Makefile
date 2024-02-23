bbbc37c53da1c67a927e6012226dee1fab0ffa46
Dmitrii Larionov <inxo@inxo.ru>
2024-02-23 14:45:36 +0300

    WP

---
+++ Makefile
@@ -0,0 +1,59 @@
+# Makefile for Go project
+
+# Environment variables
+ENV_FILE := .env
+
+# Load environment variables from .env file
+#include $(ENV_FILE)
+export
+
+# Variables
+BINARY_NAME := sync
+GO_FILES := $(wildcard *.go)
+
+# Commands
+.PHONY: build run clean
+
+build:
+	@echo "Building $(BINARY_NAME)..."
+	@go build -o build/$(BINARY_NAME) -ldflags='-extldflags "-static" -s -w' ./src/$(GO_FILES)
+
+package:
+	@echo "Package $(BINARY_NAME)..."
+	@fyne package -os darwin -icon Icon.png -name $(BINARY_NAME)App ./src/$(GO_FILES)
+
+build-win:
+	@echo "Building windows $(BINARY_NAME)..."
+	GOOS=windows GOARCH=amd64 CGO_ENABLED=0 \
+	go build -a -ldflags '-extldflags "-static" -s -w' -o build/$(BINARY_NAME).exe  ./src/$(GO_FILES)
+
+test:
+	@echo "Testing $(BINARY_NAME)..."
+	@go test ./src/$(GO_FILES)
+
+lint:
+	@golangci-lint run -v
+
+run:
+	@echo "Running $(BINARY_NAME)..."
+	@./build/$(BINARY_NAME)
+
+clean:
+	@echo "Cleaning up..."
+	@rm -f $(BINARY_NAME)
+
+# Target to run the application
+start: build run
+
+# Target to build and run the application
+restart: clean start
+
+# Help target
+help:
+	@echo "Available targets:"
+	@echo "  build    : Build the Go application"
+	@echo "  run      : Run the Go application"
+	@echo "  clean    : Clean up generated files"
+	@echo "  docs     : Generated documentation files"
+	@echo "  start    : Build and run the Go application"
+	@echo "  restart  : Clean, build, and run the Go application"
--- main.go
+++ main.go
@@ -5,54 +5,63 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	"golang.design/x/clipboard"
+	"os"
 )
 
 func main() {
 	myApp := app.New()
 	myWindow := myApp.NewWindow("File Drop App")
 
+	err := clipboard.Init()
+	if err != nil {
+		panic(err)
+	}
+
 	// Создаем виджет для отображения пути к файлу
-	filePathLabel := widget.NewLabel("Drop a file here")
+	filePathLabel := widget.NewHyperlink("Drop a file here", nil)
+	copyIconButton := widget.NewButtonWithIcon("", theme.ContentCopyIcon(), func() {
+		clipboard.Write(clipboard.FmtText, []byte(filePathLabel.URL.String()))
+	})
 
 	// Создаем дроп-зону для файла
 	dropContainer := container.New(
 		layout.NewVBoxLayout(),
-		container.NewPadded(
+		container.NewVBox(
 			widget.NewLabelWithStyle("Drop File Here", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
 			widget.NewIcon(theme.DocumentIcon()),
 		),
-		filePathLabel,
+		container.NewHBox(
+			filePathLabel,
+			copyIconButton,
+		),
 	)
 
-	//myWindow.SetOnTypedRune(func(ev *fyne.TypedRuneEvent) {
-	//	Пустая функция, чтобы окно реагировало на события клавиатуры
-	//})
+	// Если передан аргумент командной строки, используем его как путь к файлу
+	if len(os.Args) > 1 {
+		filePath := os.Args[1]
+		filePathLabel.SetText(fmt.Sprintf("File path from command line argument: %s", filePath))
+		err := filePathLabel.SetURLFromString("https://inxo.ru")
+		if err != nil {
+			dialog.ShowError(err, myWindow)
+			return
+		}
+	}
 
 	myWindow.SetOnDropped(func(pos fyne.Position, url []fyne.URI) {
-		//filePath, err := handleDropEvent(dropData)
-		//if err != nil {
-		//	dialog.ShowError(err, myWindow)
-		//	return
-		//}
 		filePath := url[0].String()
 		filePathLabel.SetText(fmt.Sprintf("Dropped file path: %s", filePath))
+		err := filePathLabel.SetURLFromString("https://inxo.ru")
+		if err != nil {
+			dialog.ShowError(err, myWindow)
+			return
+		}
 	})
 
 	myWindow.SetContent(dropContainer)
 	myWindow.ShowAndRun()
 }
-
-//func handleDropEvent(dropData fyne.DropEvent) (string, error) {
-//	if dropData == nil {
-//		return "", fmt.Errorf("invalid drop event")
-//	}
-//
-//	filePath := dropData.URI().Path()
-//	// Выводим путь к файлу
-//	log.Printf("File dropped: %s", filePath)
-//
-//	return filePath, nil
-//}
